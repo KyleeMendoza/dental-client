217ed8bb55cc536f81e703366bab4815b258d286
unknown <kylemendoza67@gmail.com>
2024-01-28 00:37:46 +0800

    appointment post started

--- src/components/ServiceDropDown.js
+++ src/components/ServiceDropDown.js
@@ -1,29 +1,36 @@
 import * as React from "react";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-export default function ServiceDropDown() {
+export default function ServiceDropDown({ label, name, value, handler }) {
+  const serviceData = [
+    { label: "Pasta Filling" },
+    { label: "Teeth Cleaning" },
+    { label: "Tooth Extraction" },
+  ];
+
   return (
-    <Autocomplete
-      disablePortal
-      id="combo-box-demo"
-      options={serviceData}
-      sx={{ width: "100%" }}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          InputLabelProps={{
-            style: { fontFamily: "Poppins, sans serif" },
-          }}
-          label="Choose a service*"
-        />
-      )}
-    />
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          name={name}
+          value={value}
+          label={label}
+          onChange={handler}
+        >
+          {serviceData.map((item, index) => (
+            <MenuItem value={item.label} key={index}>
+              {item.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
   );
 }
-
-const serviceData = [
-  { label: "Pasta Filling" },
-  { label: "Teeth Cleaning" },
-  { label: "Tooth Extraction" },
-];
--- src/pages/Client/Appoinment.js
+++ src/pages/Client/Appoinment.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ServiceDropDown from "../../components/ServiceDropDown";
 import { Button, TextField } from "@mui/material";
 import appointmentImg from "../../assets/dental-appointment.webp";
 
 function Appoinment() {
+  const [formData, setFormData] = useState({
+    phone: "",
+    date: "",
+    start_time: "",
+    service_name: "",
+    tooth_name: "",
+    additional_service: "",
+    client_note: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData); // Here, you can handle the form data as needed, e.g., send it to an API
+  };
+
   return (
     <div className="h-full relative bg-blue-50">
       <div className="h-full flex justify-center items-center border-2 border-red-600">
@@ -27,7 +50,12 @@ function Appoinment() {
               </p>
             </div>
             <div className="flex flex-col gap-2 input-container ">
-              <ServiceDropDown />
+              <ServiceDropDown
+                label="Service"
+                name={"service_name"}
+                value={formData.service_name}
+                handler={handleChange}
+              />
               <TextField
                 required
                 fullWidth
@@ -35,56 +63,69 @@ function Appoinment() {
                 InputLabelProps={{
                   style: { fontFamily: "Poppins, sans serif" },
                 }}
-                id="number"
+                id="phone"
                 label="Phone Number"
-                name="number"
-                autoComplete="number"
-                autoFocus
+                name="phone"
+                autoComplete="phone"
+                value={formData.phone}
+                onChange={handleChange}
+              />
+              <div className="flex justify-center items-center gap-1">
+                <TextField
+                  required
+                  fullWidth
+                  inputProps={{ style: { fontFamily: "Poppins, sans serif" } }}
+                  InputLabelProps={{
+                    style: { fontFamily: "Poppins, sans serif" },
+                  }}
+                  id="date"
+                  label="Choose Date"
+                  name="date"
+                  autoComplete="date"
+                  value={formData.date}
+                  onChange={handleChange}
+                />
+                <TextField
+                  required
+                  fullWidth
+                  inputProps={{ style: { fontFamily: "Poppins, sans serif" } }}
+                  InputLabelProps={{
+                    style: { fontFamily: "Poppins, sans serif" },
+                  }}
+                  id="start_time"
+                  label="Choose Time"
+                  name="start_time"
+                  autoComplete="start_time"
+                  value={formData.start_time}
+                  onChange={handleChange}
+                />
+              </div>
+              <ServiceDropDown
+                label="Add Service"
+                name={"additional_service"}
+                value={formData.additional_service}
+                handler={handleChange}
               />
               <TextField
-                required
                 fullWidth
                 inputProps={{ style: { fontFamily: "Poppins, sans serif" } }}
                 InputLabelProps={{
                   style: { fontFamily: "Poppins, sans serif" },
                 }}
-                id="date"
-                label="Choose Date"
-                name="date"
-                autoComplete="date"
-                autoFocus
-              />
-              <TextField
-                required
-                fullWidth
-                inputProps={{ style: { fontFamily: "Poppins, sans serif" } }}
-                InputLabelProps={{
-                  style: { fontFamily: "Poppins, sans serif" },
-                }}
-                id="time"
-                label="Choose Time"
-                name="time"
-                autoComplete="time"
-                autoFocus
-              />
-              <TextField
-                //
-                fullWidth
-                inputProps={{ style: { fontFamily: "Poppins, sans serif" } }}
-                InputLabelProps={{
-                  style: { fontFamily: "Poppins, sans serif" },
-                }}
-                id="note"
+                id="client_note"
                 label="Write a note"
-                name="note"
+                name="client_note"
                 multiline
                 rows={4}
-                autoComplete="note"
-                autoFocus
+                autoComplete="client_note"
+                value={formData.client_note}
+                onChange={handleChange}
               />
             </div>
             <div className="button-container flex flex-row-reverse">
-              <Button variant="contained">Submit</Button>
+              <Button variant="contained" onClick={handleSubmit}>
+                Submit
+              </Button>
             </div>
           </div>
         </div>
